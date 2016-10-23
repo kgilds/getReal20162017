f63cfa217e023d7f4ec599284f9ff188b6685362
kgilds <kevingilds@gmail.com>
2016-10-23 10:29:37 -0400

    add chunk to provide data entry status update

--- test_functions.R
+++ test_functions.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(sqldf)
-library(GetREAL)
+library(pander)
+library(getREALpackage)
+
 
 setwd("C:/Users/kevin/Dropbox/GetReal/Data/sqlite")
 
@@ -10,7 +12,13 @@ getReal_2016db<- dbConnect(SQLite(), dbname="outcome_history.sqlite")
 
 pre_outcomes <- dbReadTable(getReal_2016db, "girl_pre")
 
-hr <- pre_outcomes %>%
-    select(girlCode, council, hr.avg)
+pre_hr <- pre_outcomes %>%
+    select(girlCode, council, Time, hr.avg)
 
-hr_achieved("Pre")
+head(pre_hr)
+
+
+
+
+
+hr_achieved_pre()
